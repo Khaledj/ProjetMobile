7175b537f5628b6783c1507f743ede6768e196eb
Khaled Djhehiche <khaled.djhehiche@Digital-Grenoble.local>
2018-03-05 13:48:37 +0100

    fin étape 2

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,9 @@
   <div id="app">
     <img src="http://vuejs.org/images/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <button v-on:click="onMachinesListClick" type="button" class="btn btn-success"> Consulter la liste des machines</button>
+
+    <button v-on:click="onMapClick" type="button" class="btn btn-success"> Voir la carte</button>
   </div>
 </template>
 
@@ -24,9 +13,19 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Que voulez-vous faire ?',
+      
     }
+  },
+  methods : {
+  onMachinesListClick : function() {
+    window.alert("L'utilisateur à cliquer sur consulter la liste des machines");
+  },
+  onMapClick : function() {
+    window.alert("L'utilisateur à cliquer sur voir la carte");
   }
+}
+
 }
 </script>
 
