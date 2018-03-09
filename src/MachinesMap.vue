af3cbd97819803cfe0450128dba4d4aab347d207
Khaled Djhehiche <khaled.djhehiche@Digital-Grenoble.local>
2018-03-09 11:15:38 +0100

    rajout des marqueurs

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -1,20 +1,48 @@
 <template>
   <div >
     <h1> Carte des machines </h1>
-    <gmap-map
+
+    <gmap-map 
+  
   :center="{lat:10, lng:10}"
   :zoom="7"
   style="width: 100%; height: 800px"
 >
+
+<gmap-marker 
+:key="marker.id"
+v-for="marker in markers"
+:position="{lat: marker.latitude, lng: marker.longitude}"
+>
+</gmap-marker>
 </gmap-map>
-</div>	
+</div>
+
 </template>
 
 <script>
 
 export default {
   name: 'carte',
+  data () {
+  	return {
+markers: [{
+				id: 1,
+				latitude : 10,
+				longitude : 10,
+				
+			}, {
+				id: 2,
+				latitude: 11,
+				longitude : 9.6,
+				
+			}]
+  	        }
+         }
 
 }	
 
 </script>
+
+    
+	
